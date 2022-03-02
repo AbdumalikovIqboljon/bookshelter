d93b54b99e67527eda0856e95b3c1293325cfe4c
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-03-02 20:41:21 +0500

    Create files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import React from 'react';
+import HomePage from '../src/pages/HomePage/HomePage';
+import Login from '../src/pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage />
+      <Login />
     </div>
   );
 }
